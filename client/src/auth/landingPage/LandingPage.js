393a1403eef57f17e6613e20945f3a9fa7c70d42
Somu050600 <somasekhareega@gmail.com>
2023-05-18 01:07:42 +0530

    updated alt source for img tags

--- client/src/auth/landingPage/LandingPage.js
+++ client/src/auth/landingPage/LandingPage.js
@@ -11,11 +11,11 @@ const LandingPage = () => {
                 <div><span>Earn certifications.</span></div>
                 <div><p>Since 2014, more than 40,000 freeCodeCamp.org graduates have gotten jobs at tech companies including:</p></div>
                 <div className="images_div">
-                    <img src={process.env.PUBLIC_URL + '/images/apple.svg'} ></img>
-                    <img src={process.env.PUBLIC_URL + '/images/google.svg'} ></img>
-                    <img src={process.env.PUBLIC_URL + '/images/microsoft.svg'} ></img>
-                    <img src={process.env.PUBLIC_URL + '/images/spotify.svg'} ></img>
-                    <img src={process.env.PUBLIC_URL + '/images/amazon.svg'} ></img> 
+                    <img src={process.env.PUBLIC_URL + '/images/apple.svg'} alt="https://github.com/Somu050600/FreeCodeCamp-Assignment/assets/119160374/d45a0122-674c-4001-a482-cd8a07df81e8" ></img>
+                    <img src={process.env.PUBLIC_URL + '/images/google.svg'} alt="https://github.com/Somu050600/FreeCodeCamp-Assignment/assets/119160374/ddfb19e3-c21e-4bf9-ab3f-0d78de0c8ee3"></img>
+                    <img src={process.env.PUBLIC_URL + '/images/microsoft.svg'} alt="https://github.com/Somu050600/FreeCodeCamp-Assignment/assets/119160374/af68e919-d37a-4282-8ae0-0ff9d23ff9ae"></img>
+                    <img src={process.env.PUBLIC_URL + '/images/spotify.svg'} alt="https://github.com/Somu050600/FreeCodeCamp-Assignment/assets/119160374/3c3069fa-dbf3-422f-ac7a-705c5cd12c52"></img>
+                    <img src={process.env.PUBLIC_URL + '/images/amazon.svg'} alt="https://github.com/Somu050600/FreeCodeCamp-Assignment/assets/119160374/9c9dd112-e805-4111-bf3b-fd0bd31c0190"></img> 
                 </div>
                 <div><a href="/signin">Get Started (it's free)</a></div>
                 </div>
--- client/src/auth/navbar/AuthNavbar.js
+++ client/src/auth/navbar/AuthNavbar.js
@@ -18,12 +18,12 @@ const AuthNavbar = () => {
         <React.Fragment>
             <nav className="auth_navbar">
                 <div className="nav_search">
-                    <img src={process.env.PUBLIC_URL + '/images/search.svg'} height="15px"  alt="" />
+                    <img src={process.env.PUBLIC_URL + '/images/search.svg'} height="15px"  alt="https://github.com/Somu050600/FreeCodeCamp-Assignment/assets/119160374/74683e38-5f44-4b90-ba48-e1a3382af715" />
                     <input type="text" placeholder="Search 9000+ tutorials"/>
                 </div>
                 <div onClick={homeHanler} className="nav_title">
                     {/* <span>FreeCodeCamp</span> */}
-                    <img src={process.env.PUBLIC_URL + '/images/FreeCodeCamp.svg'} alt="" />
+                    <img src={process.env.PUBLIC_URL + '/images/FreeCodeCamp.svg'} alt="https://github.com/Somu050600/FreeCodeCamp-Assignment/assets/119160374/96a1691d-8af6-4e55-b3f3-f7daa3dd496d" />
                 </div>
                 <div className="nav_menu">
                     <span>Menu</span>
